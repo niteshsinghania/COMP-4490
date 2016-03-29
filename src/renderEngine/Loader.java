56168bbbde215451767ff7e4632f84297ddc7b49
niteshsinghania <niteshsinghania@hotmail.com>
2016-03-29 03:25:26 -0500

    Started Skybox.

--- src/renderEngine/Loader.java
+++ src/renderEngine/Loader.java
@@ -3,6 +3,7 @@ package renderEngine;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 import java.util.ArrayList;
@@ -12,12 +13,17 @@ import models.RawModel;
 
 import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
 import org.lwjgl.opengl.GL14;
 import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
 import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.opengl.TextureLoader;
+
+import de.matthiasmann.twl.utils.PNGDecoder;
+import de.matthiasmann.twl.utils.PNGDecoder.Format;
+import textures.TextureData;
 
 public class Loader {
 	
@@ -59,6 +65,41 @@ public class Loader {
 		for(int texture:textures){
 			GL11.glDeleteTextures(texture);
 		}
+	}
+	
+	public int loadCubeMap(String[] textureFiles){
+		int texID = GL11.glGenTextures();
+		GL13.glActiveTexture(GL13.GL_TEXTURE0);
+		GL11.glBindTexture(GL13.GL_TEXTURE_CUBE_MAP, texID);
+		
+		for (int i = 0; i < textureFiles.length; i++) {
+			TextureData data = decodeTextureFile("res/"+ textureFiles[i] +".png");
+			GL11.glTexImage2D(GL13.GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL11.GL_RGBA, data.getWidth(), data.getHeight(), 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, data.getBuffer());
+		}
+		GL11.glTexParameteri(GL13.GL_TEXTURE_CUBE_MAP, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+		GL11.glTexParameteri(GL13.GL_TEXTURE_CUBE_MAP, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+		textures.add(texID);
+		return texID;
+	}
+	
+	private TextureData decodeTextureFile(String filename){
+		int width = 0;
+		int height = 0;
+		ByteBuffer buffer = null;
+		
+		try {
+			FileInputStream in = new FileInputStream(filename);
+			PNGDecoder decoder = new PNGDecoder(in);
+			width = decoder.getHeight();
+			height = decoder.getHeight();
+			buffer = ByteBuffer.allocateDirect(4*width*height);
+			decoder.decode(buffer, width*4, Format.RGBA);
+			buffer.flip();
+			in.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return new TextureData(buffer, width,height);
 	}
 	
 	private int createVAO(){
